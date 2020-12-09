6cc95a3c97156e872bbdaaff784ae27ac0671905
Slavat <tymosh@gmail.com>
2020-12-09 21:28:57 +0100

    add files and styles

---
+++ src/js/myCode.js
@@ -0,0 +1,25 @@
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"'),
+    stopBtn: document.querySelector('button[data-action="stop"'),
+  },
+  colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'],
+  randomIntegerFromInterval = max => {
+    return Math.floor(Math.random() * (max + 1));
+  },
+  setRandomColor = () => {
+    const _color = colors[randomIntegerFromInterval(colors.length - 1)];
+    console.log(_color);
+    document.body.style.backgroundColor = _color;
+  };
+
+let interval = undefined;
+
+refs.startBtn.addEventListener(
+  'click',
+  e =>
+    (interval = interval
+      ? interval
+      : setInterval(() => setRandomColor(), 1000)),
+);
+
+refs.stopBtn.addEventListener('click', e => clearInterval(interval));
